1709995179805dea28dc98c51aa15139a3f8bb97
darya.gembitskaya <daxagemb@gmail.com>
2019-03-18 20:18:51 +0300

    feat:add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for(let i = 0; i < str.length; i++){
+      if(str[i] === '(' || str[i] === '[' || str[i] === '{' ){
+        stack.push(str[i]);
+      }
+      if(str[i] === ')' && stack[stack.length - 1] === '('){
+          stack.pop();
+      }
+      if(str[i] === '}' && stack[stack.length - 1] === '{'){
+          stack.pop();
+      }
+      if(str[i] === ']' && stack[stack.length - 1] === '['){
+          stack.pop();
+      }
+  }
+  return stack.empty;
 }
